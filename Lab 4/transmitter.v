7087f8a348adc7924c39614c3b9c7a93e6550d1c
Douglas Zorn <dzorn@ucsc.edu>
2014-02-28 11:50:49 -0800

    Lab 4 initial transmitter
    Initial commit for Lab 4 transmitter

---
+++ Lab 4/transmitter.v
@@ -0,0 +1,13 @@
+module transmitter(
+	input pclk_i,
+	input prst_n_i,
+	input [7:0] tx_pdata_i,
+	input tx_pdata_valid_i,
+	input tx_sclk_n_i,
+	input tx_srst_n_i,
+	output tx_sdata_o,
+	output tx_pready_o
+);
+
+
+endmodule
