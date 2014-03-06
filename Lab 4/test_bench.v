5afe151378bcd6b7970a3d0b2b98b4411a4947e8
Doug <namelessone.doug@gmail.com>
2014-03-06 12:59:24 -0800

    Master reset for all clock domains

--- Lab 4/test_bench.v
+++ Lab 4/test_bench.v
@@ -3,9 +3,10 @@ module test_bench(
 	input p_clk_i,
 	input tx_sclk_i,
 	input rx_sclk_i,
-	output reg prst_n_o,
-	output reg rx_srst_n_o, 
-	output reg tx_srst_n_o,
+	output reg master_rst_o,
+	//output reg prst_n_o,
+	//output reg rx_srst_n_o, 
+	//output reg tx_srst_n_o,
 	output reg tx_pdata_valid_o,
 	output reg [7:0] tx_pdata_o
 );
@@ -14,12 +15,12 @@ initial
 	begin
 		tx_pdata_valid_o = 1'b0; //initial values
 
-		tx_srst_n_o = 1'b1;
-		@(negedge tx_sclk_i);
-		tx_srst_n_o = 1'b0;
-		repeat (2) @(negedge tx_sclk_i);
-		tx_srst_n_o = 1'b1;
-
+		master_rst_o = 1'b1;
+		@(negedge p_clk_i);
+		master_rst_o = 1'b0;
+		repeat (2) @(negedge p_clk_i);
+		master_rst_o = 1'b1;
+/*
 		prst_n_o = 1'b1;
 		@(negedge p_clk_i);
 		prst_n_o = 1'b0;
@@ -32,6 +33,7 @@ initial
 		tx_pdata_valid_o = 1'b1;
 		@(negedge tx_sclk_i);
 		tx_pdata_valid_o = 1'b0;
+		
 
 		repeat (2) @(negedge rx_sclk_i);
 		rx_srst_n_o = 1'b1;
@@ -39,7 +41,16 @@ initial
 		rx_srst_n_o = 1'b0;
 		@(negedge rx_sclk_i);
 		rx_srst_n_o = 1'b1;
+		*/
 		
+		repeat (4) @(posedge p_clk_i);
+		tx_pdata_o = 8'd197;
+		
+		
+		@(negedge p_clk_i);
+		tx_pdata_valid_o = 1'b1;
+		//@(negedge p_clk_i);
+		//tx_pdata_valid_o = 1'b0;
 
 
 	end //initial
