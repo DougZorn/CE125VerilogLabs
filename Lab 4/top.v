42a43389b370a21e243756108333c27ea49b0f93
Doug <namelessone.doug@gmail.com>
2014-03-06 12:59:50 -0800

    Master reset wire changes

--- Lab 4/top.v
+++ Lab 4/top.v
@@ -14,6 +14,7 @@ wire [9:0] wdata_w;
 wire winc_w;
 wire rx_srst_n_w;
 wire tx_pready_w;
+wire master_rst_w;
 
 clock_gen #(.CLOCK_PERIOD(1000)) clock_gen_p_clk_0 (.clock_i(p_clk_w));
 clock_gen #(.CLOCK_PERIOD(100)) clock_gen_tx_sclk_0 (.clock_i(tx_sclk_w));
@@ -25,18 +26,19 @@ test_bench test_bench_0(
 	.p_clk_i(p_clk_w),
 	.tx_sclk_i(tx_sclk_w),
 	.rx_sclk_i(rx_sclk_w),
-	.rx_srst_n_o(rx_srst_n_w),
-	.prst_n_o(prst_n_w),
-	.tx_srst_n_o(tx_srst_n_w)
+	//.rx_srst_n_o(rx_srst_n_w),
+	//.prst_n_o(prst_n_w),
+	//.tx_srst_n_o(tx_srst_n_w)
+	.master_rst_o(master_rst_w)
 );
 
 uart_tx uart_tx_0(
 	.pclk_i(p_clk_w),
-	.prst_n_i(prst_n_w),
+	.prst_n_i(master_rst_w),
 	.tx_pdata_i(tx_pdata_w),
 	.tx_pdata_valid_i(tx_pdata_valid_w),
 	.tx_sclk_i(tx_sclk_w),
-	.tx_srst_n_i(tx_srst_n_w),
+	.tx_srst_n_i(master_rst_w),
 	.tx_sdata_o(tx_2_rx_s_data_w),
 	.tx_pready_o(tx_pready_w)
 );
@@ -44,7 +46,7 @@ uart_tx uart_tx_0(
 uart_rx uart_rx_0(
 	.rx_sclk_i(rx_sclk_w),
 	.rx_data_i(tx_2_rx_s_data_w),
-	.rx_srst_n_i(rx_srst_n_w),
+	.rx_srst_n_i(master_rst_w),
 	.wdata_o(wdata_w), 
 	.winc_o(winc_w)       
 );
